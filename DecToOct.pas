88f810c2cf90b8cabaa76113e00a485e278089a4
Alexander <pchp250g8@tutamail.com>
2026-04-10 11:03:51 +0300

    Created Project DecToOct

---
+++ DecToOct.pas
@@ -0,0 +1,33 @@
+﻿uses
+  System;
+const
+  MAX_INT = Cardinal.MaxValue;
+begin
+  var uNumber : Cardinal;
+  Console.WriteLine('Input an unsigned integer number');
+  var bIsRightNumber := (Cardinal.TryParse(Console.ReadLine(), uNumber)) and
+                     (uNumber <= Cardinal.MaxValue);
+  if (not bIsRightNumber) then
+  begin
+    Console.WriteLine('Invalid number format or number too big');
+    Console.Read();
+    exit();
+  end;
+  var strOctNum := '';
+  var uTempVal := uNumber;
+  while (uTempVal > 0) do
+  begin
+    strOctNum := uTempVal mod 8 + strOctNum;
+    uTempVal := uTempVal div 8;
+  end;
+  if (strOctNum.Length = 0) then
+  begin
+    strOctNum := '0';
+  end;
+  Console.WriteLine
+  (
+    'The octal equavalent of the decimal number {0} is: {1}',
+    uNumber, strOctNum
+  );
+  Console.Read();
+end.
